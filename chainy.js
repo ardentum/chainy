5a370681f7fd1df5c0866b016445e795decc60ae
Ivanov Pavel <ardentum@gmail.com>
2012-07-12 19:56:34 +0400

    wrong version replaced :3

--- chainy.js
+++ chainy.js
@@ -1,85 +1,146 @@
-var Chain = function (parent, state) {
-    var self = this;
+(function (exports) {
+    var Chain = function (parent) {
+        var chain = this;
 
-    self._state = state || self._states.PROCESS;
+        chain._parent = parent || chain;
+        chain._emitter = new EventEmitter();
+        chain._size = 0;
+        chain._completedCount = 0;
 
-    self._stateData = [];
-    self._emitter = new EventEmitter();
-    self._parent = parent;
+        chain._emitter.on("newListener", function (state, work) {
+            if (state === chain._state) {
+                work();
+            }
+        });
+    };
 
-    self._emitter.on("newListener", function (type, listener) {
-        if (self._state === type) {
-            listener.apply(self.stateData);
+    Chain.prototype.states = {
+        DONE: "done",
+        FAIL: "fail"
+    };
+
+    /**
+     * Adds a function in the chain.
+     * @param  {Function} callback callback function
+     * @param  {Array}    args     arguments array
+     * @return {Chain}
+     */
+    Chain.prototype.call = function (callback, args, state) {
+        var chain = this,
+            subChain = new Chain(chain);
+
+        chain.position = subChain;
+        chain._size++;
+
+        chain._emitter.once(state || chain._readyState || Chain.prototype.states.DONE, function (data) {
+            subChain.data = data;
+
+            setTimeout(function () {
+                callback.apply(subChain, args);
+            }, 0);
+        });
+
+        return chain;
+    };
+
+    /**
+     * Adds a function or a new subchain to be 
+     * executed after the previous call will be done.
+     *
+     * @param {function} callback  callback function
+     * @param {Array}    args      arguments array
+     *
+     * @example
+     *   .whenDone(foo, argsArray)
+     *
+     * @example
+     *   .whenDone()
+     *     .call(foo, argsArray)
+     *     .call(foo, argsArray)
+     *   .end 
+     *
+     * @return Chain
+     */
+    Chain.prototype.when = function (state, callback, args) {
+        var subChain;
+
+        if (arguments.length === 3) {
+            // single call
+            return this.position.call(callback, args, state).end();
+        } else {
+            // multiple calls
+            subChain = this.position;
+            subChain._readyState = state;
+
+            return subChain;
         }
-    });
-};
+    };
 
-Chain.prototype._states = {
-    PROCESS: "process",
-    DONE:    "done",
-    FAIL:    "fail" 
-};
-
-Chain.prototype._set = function (state, result) {
-    this._state = state;
-    this._emitter.emit(state, result);
-};
-
-Chain.prototype.do = function (callback, args) {
-    var chain = new Chain(this);
-
-    callback.apply(chain, args);
-    return chain;
-};
-
-Chain.prototype.then = function (callback, args) {
-    var chain = new Chain(this);
-
-    this._emitter.on(this._states.DONE, function (result) {
-        chain.result = result;
-        callback.apply(chain, args);
-    });
-
-    return chain;
-};
-
-Chain.prototype.and = function () {
-    if (this._parent) {
-        return this._parent.then.apply(this._parent, arguments);
-    }
-};
-
-Chain.prototype.end = function () {
-    if (this._parent) {
+    /**
+     * Returns parent chain.
+     * @return {Chain}
+     */
+    Chain.prototype.end = function () {
         return this._parent;
-    }
-};
+    };
 
-Chain.prototype.fail = function (result) {
-    this._set(this._states.FAIL, result);
-};
+    Chain.prototype._set = function (state, data) {
+        this._state = state;
+        this._parent._completedCount++;
+        this._emitter.emit(state, data);
 
-Chain.prototype.done = Chain.prototype.next = function (result) {
-    this._set(this._states.DONE, result);
-};
+        if (this._parent._completedCount === this._parent._size) {
+            this._parent._emitter.emit("allDone");
+        }
 
-var chain = function () {
-    return new Chain(null, Chain.prototype._states.DONE);
-};
+        return this;
+    };
 
-var foo = function (num) {
-    var chain = this;
+    /**
+     * Announces the successful completion of an asynchronous function
+     * 
+     * @param  {misc}   data data to be sent to success callbacks
+     * @return {Chain}
+     */
+    Chain.prototype.next = Chain.prototype.done = function (data) {
+        return this._set(Chain.prototype.states.DONE, data);
+    };
 
-    setTimeout(function () {
-        var result = num + (chain.result || 0);
-        console.log(num, result);
-        chain.next(result);
-    }, 1000);
-};
+    Chain.prototype.fail = function (data) {
+        return this._set(Chain.prototype.states.FAIL, data);
+    };
 
+    /**
+     * Creates a callback block as a single callback that will be
+     * successfully completed when all callbacks will be completed.
+     *
+     * @example
+     * .together()
+     *    .call(foo1, argsArray)
+     *    .call(foo2, argsArray)
+     * .end()
+     * 
+     * @return {Chain} [description]
+     */
+    Chain.prototype.together = function () {
+        var chain = this,
+            chainBlock = new Chain(),
+            result;
 
-chain()
-    .do(foo, [1])
-        .then(foo, [2])
-        .end()
-    .and(foo, [3]);
+        result = this.call(function () {
+            var self = this;
+
+            chainBlock.done()._emitter.on("allDone", function () {
+                self.done();
+            });
+        });
+
+        chainBlock._parent = result;
+
+        return chainBlock;
+    };
+
+    exports.chainy = function () {
+        return new Chain().done();
+    };
+}(typeof module === "object" ? module.exports : window));
